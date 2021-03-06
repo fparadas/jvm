45dd8dc71abd9e4921cfe85cdaa3a1747000a9c5
fparadas <felipegparadas@gmail.com>
2021-03-06 08:32:29 -0300

    added Classfile specs

---
+++ src/dotclass.cpp
@@ -0,0 +1,26 @@
+#include <types.h>
+
+typedef struct classfile{
+    u4 magic;
+    u2 minor_version;
+    u2 major_version;
+    
+    u2 cp_count;
+    cp_info* cp;
+
+    u2 access_flags;
+    u2 this_class;
+    u2 super_class;
+    
+    u2 interfaces_count;
+    u2* interfaces;
+
+    u2 fields_count;
+    field_info* fields;
+
+    u2 methods_count;
+    method_info* methods;
+
+    u2 attributes_count;
+    attribute_info* attributes;
+};
---
+++ src/types.h
@@ -0,0 +1,8 @@
+typedef unsigned char u1;
+typedef unsigned short u2;
+typedef unsigned int u4;
+
+typedef struct cp_info {}; //#TODO
+typedef struct field_info {}; //#TODO
+typedef struct method_info {}; //#TODO
+typedef struct attribute_info {}; //#TODO
